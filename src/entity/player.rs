4ad6605906a4b7023665869f11573f84e4b7ef09
lolbitto <rafae.juck@gmail.com>
2025-07-25 08:29:39 -0300

    Movimentação no eixo X relativo a camera arrumado

--- src/entity/player.rs
+++ src/entity/player.rs
@@ -42,23 +42,13 @@ impl Player {
         let x = self.mira[0];
         let z = self.mira[2];
  
-        let cat_adj = x - self.position[0];
-        let cat_op  = z - self.position[2];
+        let x_ratio = x - self.position[0];
+        let z_ratio  = z - self.position[2];
 
-        let raio = libm::sqrtf(libm::powf(cat_op, 2.0) + libm::powf(cat_adj, 2.0));
+        let new_x_z = rotacionar_ponto_x(x_ratio, z_ratio, angle);
 
-        let ang_rad = libm::atan2f(cat_op, cat_adj);
-
-        let ang_final = (ang_rad * 57.2958 - angle) / 57.2958; // rad * 57.2958 + angulo (graus), dps
-                                                              // divide para ir em radianos dnv         
-        let mut new_x = 0.0;
-        let mut new_z = 0.0;
-        
-        new_x = libm::cosf(ang_final) * raio;
-        new_z = libm::sinf(ang_final) * raio;
-
-        self.mira[0] = new_x + self.position[0];
-        self.mira[2] = new_z + self.position[2];
+        self.mira[0] = new_x_z[0] + self.position[0];
+        self.mira[2] = new_x_z[1] + self.position[2];
 
         println!("*mira* x: {}, y: {}, z: {}", self.mira[0], self.mira[1], self.mira[2]);
     }
@@ -87,13 +77,13 @@ impl Player {
     }
 
     pub fn move_relative_z(&mut self, speed: f32) {
-        let mira_x = self.mira[0] - self.position[0];
-        let mira_z = self.mira[2] - self.position[2];
+        let mira_ratio_x = self.mira[0] - self.position[0];
+        let mira_ratio_z = self.mira[2] - self.position[2];
 
-        let hip = libm::sqrtf(libm::powf(mira_x, 2.0) + libm::powf(mira_z, 2.0));
+        let hip = libm::sqrtf(libm::powf(mira_ratio_x, 2.0) + libm::powf(mira_ratio_z, 2.0));
     
-        let sen = mira_z / hip;
-        let cos = mira_x / hip;
+        let sen = mira_ratio_z / hip;
+        let cos = mira_ratio_x / hip;
 
         let x = cos * speed;
         let z = sen * speed;
@@ -106,16 +96,18 @@ impl Player {
     }
 
     pub fn move_relative_x(&mut self, speed: f32) {
-        let mira_x = self.mira[0] - self.position[0];
-        let mira_z = self.mira[2] - self.position[2];
+        let mira_ratio_x = self.mira[0] - self.position[0];
+        let mira_ratio_z = self.mira[2] - self.position[2];
 
-        let hip = libm::sqrtf(libm::powf(mira_x, 2.0) + libm::powf(mira_z, 2.0));
+        let new_mira_x_z = rotacionar_ponto_x(mira_ratio_x, mira_ratio_z, libm::asinf(1.0));
+
+        let hip = libm::sqrtf(libm::powf(new_mira_x_z[0], 2.0) + libm::powf(new_mira_x_z[1], 2.0));
     
-        let sen = mira_z / hip;
-        let cos = mira_x / hip;
+        let sen = new_mira_x_z[1] / hip;
+        let cos = new_mira_x_z[0] / hip;
 
-        let z = cos * speed;
-        let x = sen * speed;
+        let x = cos * speed;
+        let z = sen * speed;
 
         self.position[0] += x;
         self.position[2] += z;
@@ -128,20 +120,7 @@ impl Player {
         let width_ratio = meio.x - position.x;
         let height_ratio = meio.y - position.y;
 
-        let mut x_inc = width_ratio / width_ratio;
-        let mut y_inc = height_ratio / height_ratio; // Parece mais ez definir como 1, mas e se for 0,
-                                                 // daí é mais um if, mô empenho slk. Assim é mais
-                                                 // prático
-
-        if width_ratio < 1.0 {
-            x_inc *= -1.0;
-        }
-
-        if height_ratio < 1.0 {
-            y_inc *= -1.0;
-        }
-        
-        self.rotate_view_x(-width_ratio as f32);
+        self.rotate_view_x(width_ratio as f32 / 100.0);
         //self.rotate_view_y(-height_ratio as f32);
 
     }
