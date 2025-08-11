028b764fc5dffa4f30f69c551a3406d01a79a32c
lolbitto <rafae.juck@gmail.com>
2025-08-11 10:44:27 -0300

    Começo da implementação do menu de seleção de mapas

--- src/states/map_selector_state.rs
+++ src/states/map_selector_state.rs
@@ -1,5 +1,106 @@
-mod map_selector_state;
+use crate::states::State;
+use crate::resources::file_manager::get_resource_list;
+use crate::resources::file_manager::Resource;
+
+use winit::event::KeyEvent;
+use winit::keyboard::{PhysicalKey, KeyCode};
+use winit::event::ElementState;
+use winit::event::MouseButton;
+use winit::dpi::{PhysicalPosition, PhysicalSize};
 
 struct MapSelectorState {
+    maps: Vec<String>,
+    selector: u32,
+    key_manager: KeyManager,
+    vertices: Vec<f32>
+}
+
+struct KeyManager {
+    pub up: bool,
+    pub down: bool
+}
+
+impl MapSelectorState {
+    pub fn new() -> Self {
+        let maps = get_resource_list(Resource::Map);
+        let selector = 0;
+        let key_manager = KeyManager {up: false, down: false};
+        let vertices = vec![0.0];
+
+        Self{maps, selector, key_manager, vertices}
+    }
+}
+
+impl State for MapSelectorState {
     
+    fn get_vertices (&self) -> &Vec<f32> {
+        &self.vertices
+    }
+
+    fn update(&mut self) {
+        let down = self.key_manager.down;
+        let up = self.key_manager.up ;
+        match true {
+            up => {
+                if self.selector == self.maps.len() as u32 - 1 {
+                    self.selector = 0;
+                } else {
+                    self.selector += 1;
+                }
+            },
+
+            down => {
+                if self.selector == 0 {
+                    self.selector = self.maps.len() as u32 - 1;
+                } else {
+                    self.selector -= 1;
+                }
+            }
+        }
+    }
+
+    fn manage_keyboard_input(&mut self, event: KeyEvent) {
+        if !event.repeat {
+            let tecla = event.physical_key;
+
+            match tecla {
+                PhysicalKey::Code(code) => {
+                    match code {
+                        KeyCode::ArrowUp => {
+                            if event.state == ElementState::Pressed {
+                                self.key_manager.up = true;
+                            } else {
+                                self.key_manager.up = false;
+                            }
+                        }
+
+                        KeyCode::ArrowDown => {
+                            if event.state == ElementState::Pressed {
+                                self.key_manager.down = true;
+                            } else {
+                                self.key_manager.down = false;
+                            }
+                        },
+
+                        _ => {}
+                    }
+                },
+
+                _ => {}
+            }
+        }
+    }
+
+    fn manage_mouse_input(&mut self, button: MouseButton) {
+    
+    }
+
+    fn manage_mouse_movement(&mut self, position: PhysicalPosition<f64>) {
+
+    }
+    
+    fn manage_window_resize(&mut self, size: PhysicalSize<u32>) {
+
+    }
+
 }
